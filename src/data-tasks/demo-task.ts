c206e2e2fbfd0cdae1dd013f312f6e2591828875
yh360081 <yh360081@alibaba-inc.com>
2025-09-01 02:45:44 +0800

    demo-task

---
+++ src/data-tasks/demo-task.ts
@@ -0,0 +1,79 @@
+import type { AcquisitionHandler, AnalysisHandler, OutputHandler } from "../types";
+import { PageActionAcquisitionHandler } from "../data-acquisition/page-action-acquisition.js";
+import { BaseTask } from "./base-task.js";
+import { ConsoleOutputHandler } from "../data-output/console-output.js";
+import { PageScreenAcquisitionHandler } from "../data-acquisition/page-screen-acquisition.js";
+import { UrlUtils } from "../utils/url-utils.js";
+import { HtmlOutputHandler } from "../data-output/html-output.js";
+import { PageScrapeAcquisitionHandler, TaskProgressListener } from "../data-acquisition/page-scrape-acquisition.js";
+import { TextOutputHandler } from "../data-output/text-output.js";
+import { LlmAnalysisHandler } from "../data-analysis/llm-analysis.js";
+import { DeepSeekService } from "../services/llm/deep-seek.js";
+import { IdeaLabService } from "../services/llm/idea-lab.js";
+
+/**
+ * demo任务
+ */
+export class DemoTask extends BaseTask {
+
+  /**
+   * 获取采集处理器
+   * @returns 采集处理器实例
+   */
+  protected getAcquisitionHandler(): AcquisitionHandler {
+    // 采集任务监听器
+    const progressListener: TaskProgressListener = {
+      // 单个采集任务开始前回调
+      onTaskStart: (taskIndex, task) => {
+        if (!task || task.url == null) {
+          return;
+        }
+
+        let targetUrl = task.url;
+        targetUrl = UrlUtils.updateUrlParams(targetUrl, {
+          "search": "test",
+        })
+        task.url = targetUrl;
+      },
+    };
+    return new PageActionAcquisitionHandler(progressListener);
+  }
+
+  /**
+  * 获取分析处理器
+  * @returns 分析处理器实例（可选）
+  */
+  // protected getAnalysisHandler(): AnalysisHandler {
+  //   const systemPrompt = '你是资深的新闻工作者，对热点新闻有深刻的洞察力';
+  //   let chatPrompt = "你需要根据提供的数据完成以下的工作:\n";
+  //   chatPrompt += '1.对提供的新闻进行分析总结并给出独到的见解\n';
+  //   chatPrompt += '请以易于阅读的形式返回分析结果。';
+
+  //   return new LlmAnalysisHandler(new DeepSeekService(), { systemPrompt: systemPrompt, chatPrompt: chatPrompt });
+  //   // return new LlmAnalysisHandler(new IdeaLabService(), { systemPrompt: systemPrompt, chatPrompt: chatPrompt });
+  // }
+
+  /**
+  * 获取输出处理器
+  * @returns 输出处理器实例（可选）
+  */
+  protected getOutputHandler(): OutputHandler {
+    return new ConsoleOutputHandler();
+  }
+
+  /**
+   * 任务描述
+   * @returns 
+   */
+  protected getTaskDescription(): string {
+    return "demo任务";
+  }
+
+  /**
+   * 使用方式
+   * @returns 
+   */
+  protected getUsage(): string {
+    return "npm run task demo <confignName>";
+  }
+}
